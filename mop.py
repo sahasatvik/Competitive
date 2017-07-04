912451cf2893ef4bad2a0a83f3a916d57b503c71
Satvik Saha <sahasatvik@gmail.com>
2017-07-04 20:51:08 +0530

    Refactored mop.py -> ones

--- mop.py
+++ mop.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # Displays the shortest string of multiplication signs (x's), ones (1's)
 # and plusses (+'s) that evaluates to the number passed to it.
@@ -8,21 +9,21 @@
 import sys
 
 def mop_exp(n):
-    if n < 1 :                          # invalid cases
+    if n < 1:                           # invalid cases
         return ''
-    elif n in [1, 2, 3, 5] :            # base cases
+    elif n in [1, 2, 3, 5]:             # base cases
         return ones(n)
-    for i in [2, 3, 5] :                # for multiples of the base cases, factor them out
-        if (n % i) == 0 :
+    for i in [2, 3, 5]:                 # for multiples of the base cases, factor them out
+        if (n % i) == 0:
             return '(' + ones(i) + ') x (' + mop_exp(n / i) + ')'
     return '1 + ' + mop_exp(n - 1)      # recurse by pulling out a one (1)
 
 def ones(n):                            # generate a string of ones (1's) added with each other
-    return '1' + (' + 1' * (n - 1))
+    return ' + '.join('1' * n)
 
 def count_ones(s):                      # count the number of ones (1's) in a string
-    return len(filter( lambda a: a == '1', s))
+    return len(filter(lambda a: a == '1', s))
 
-exp = mop_exp(int(sys.argv[1]))          # call mop_exp on cli input
+exp = mop_exp(int(sys.argv[1]))         # call mop_exp on cli input
 print 'MOPExp : {}'.format(exp)
 print 'Length : {}'.format(count_ones(exp))
